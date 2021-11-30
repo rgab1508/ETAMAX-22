d152eb72fed55f1a36946395d4232de42a381436
AnushkaAmte <anushkaamte@gmail.com>
2021-11-30 23:06:23 +0530

    chnaged color

--- client/pages/login.js
+++ client/pages/login.js
@@ -21,10 +21,10 @@ const login = () => {
       </Head>
       <Box alignItems="center" borderRadius="lg">
         <Center>
-          <Tabs isFitted variant="enclosed">
-            <TabList>
-              <Tab fontSize={"xl"}>FCRIT Students</Tab>
-              <Tab fontSize={"xl"}>Other Colleges</Tab>
+          <Tabs isFitted variant="enclosed" color="pink.800">
+            <TabList color="pink.800">
+              <Tab fontSize={"xl"} color="pink.800">FCRIT Students</Tab>
+              <Tab fontSize={"xl"} color="pink.800">Other Colleges</Tab>
             </TabList>
             <TabPanels>
               <TabPanel>
