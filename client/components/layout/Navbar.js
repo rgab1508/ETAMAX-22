6ec9dfe0d3860c55acb9452c62f5d241d0b4e3ff
Gabriel <rgabriel1508@gmail.com>
2022-02-06 20:30:35 +0530

    added custom-scrollbar to body & added Checkout NavItem

--- client/components/layout/Navbar.js
+++ client/components/layout/Navbar.js
@@ -9,6 +9,7 @@ import {
   Stack,
   useColorModeValue,
   useToast,
+  createIcon,
 } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import LinkButtons from "./LinkButtons";
@@ -41,6 +42,18 @@ function Logo({ visible, ...rest }) {
     </Center>
   );
 }
+
+const ShoppingCartIcon = createIcon({
+  displayName: "shopping cart",
+  viewBox: "0 0 576 512",
+  // path can also be an array of elements, if you have multiple paths, lines, shapes, etc.
+  path: (
+    <path
+      fill="currentColor"
+      d="M528.12 301.319l47.273-208C578.806 78.301 567.391 64 551.99 64H159.208l-9.166-44.81C147.758 8.021 137.93 0 126.529 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24h69.883l70.248 343.435C147.325 417.1 136 435.222 136 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-15.674-6.447-29.835-16.824-40h209.647C430.447 426.165 424 440.326 424 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-22.172-12.888-41.332-31.579-50.405l5.517-24.276c3.413-15.018-8.002-29.319-23.403-29.319H218.117l-6.545-32h293.145c11.206 0 20.92-7.754 23.403-18.681z"
+    />
+  ),
+});
 
 function MenuToggle({ toggle }) {
   return (
@@ -181,6 +194,14 @@ function DrawerNavbar({ isOpen }) {
               {loggedIn && (
                 <MenuItems color={color} to="/profile">
                   Profile
+                </MenuItems>
+              )}
+              {loggedIn && (
+                <MenuItems color={color} to="/checkout">
+                  <Flex gridGap={"1"}>
+                    <Box>Checkout</Box>
+                    <ShoppingCartIcon />
+                  </Flex>
                 </MenuItems>
               )}
               {loggedIn && (
